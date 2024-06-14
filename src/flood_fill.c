936965313f18b70f1d0f1047a83c18279db34380
nyzss <oknakoca@gmail.com>
2024-06-14 23:17:13 +0200

    feat: made the flood fill algorithm work by changing the order in which I fill the directions lol

--- src/flood_fill.c
+++ src/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 22:30:44 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/14 23:07:34 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/14 23:16:00 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int	sl_handle_char(char *c)
 {
-	if (*c != FILL_CHAR && *c != WALL_CHAR)
+	if (*c != FILL_CHAR && *c != WALL_CHAR && *c != EXIT_CHAR)
 	{
 		*c = 'F';
 		return (1);
@@ -24,46 +24,11 @@ int	sl_handle_char(char *c)
 	return (0);
 }
 
-// int	sl_handle_down(char *c)
-// {
-// 	if (*c != FILL_CHAR && *c != WALL_CHAR)
-// 	{
-// 		*c = 'F';
-// 		return (1);
-// 	}
-// 	return (0);
-// }
-
-// int	sl_handle_left(char *c)
-// {
-// 	if (*c != FILL_CHAR && *c != WALL_CHAR)
-// 	{
-// 		*c = 'F';
-// 		return (1);
-// 	}
-// 	return (0);
-// }
-
-// int	sl_handle_right(char *c)
-// {
-// 	if (*c != FILL_CHAR && *c != WALL_CHAR)
-// 	{
-// 		*c = 'F';
-// 		return (1);
-// 	}
-// 	return (0);
-// }
-
 void	sl_flood_fill(t_vec2 p_pos, char **arr)
 {
-	if (sl_handle_char(&(arr[p_pos.pos_y][p_pos.pos_x + 1])) == 1)
+	if (sl_handle_char(&(arr[p_pos.pos_y - 1][p_pos.pos_x])) == 1)
 	{
-		p_pos.pos_x += 1;
-		sl_flood_fill(p_pos, arr);
-	}
-	if (sl_handle_char(&(arr[p_pos.pos_y][p_pos.pos_x - 1])) == 1)
-	{
-		p_pos.pos_x -= 1;
+		p_pos.pos_y -= 1;
 		sl_flood_fill(p_pos, arr);
 	}
 	if (sl_handle_char(&(arr[p_pos.pos_y + 1][p_pos.pos_x])) == 1)
@@ -71,9 +36,14 @@ void	sl_flood_fill(t_vec2 p_pos, char **arr)
 		p_pos.pos_y += 1;
 		sl_flood_fill(p_pos, arr);
 	}
-	if (sl_handle_char(&(arr[p_pos.pos_y - 1][p_pos.pos_x])) == 1)
+	if (sl_handle_char(&(arr[p_pos.pos_y][p_pos.pos_x - 1])) == 1)
 	{
-		p_pos.pos_y -= 1;
+		p_pos.pos_x -= 1;
+		sl_flood_fill(p_pos, arr);
+	}
+	if (sl_handle_char(&(arr[p_pos.pos_y][p_pos.pos_x + 1])) == 1)
+	{
+		p_pos.pos_x += 1;
 		sl_flood_fill(p_pos, arr);
 	}
 }
