0f1ec56ce3a659f727bd1caf74e6a224156476f1
Okan Emre KOCA <oknakoca@gmail.com>
2024-06-14 17:54:36 +0200

    wip check for maximum number of player/exit

--- src/check.c
+++ src/check.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 11:16:54 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/14 15:49:47 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/14 16:09:50 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,24 @@ int	sl_check_rectangular(char **arr)
 	return (0);
 }
 
+int	sl_check_max_char(char **arr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (arr[i])
+	{
+		j = 0;
+		while (arr[i][j])
+		{
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
 void	sl_check_map(char **arr)
 {
 	if (sl_check_invalid_char(arr) != 0)
@@ -75,9 +93,9 @@ void	sl_check_map(char **arr)
 		sl_clear_map(arr);
 		sl_error_exit(EXIT_FAILURE, "Map is not closed by walls.");
 	}
-	// else if (sl_check_max_char(arr) != 0)
-	// {
-	// 	sl_clear_map(arr);
-	// 	sl_error_exit(EXIT_FAILURE, "Map is not rectangular.");
-	// }
+	else if (sl_check_max_char(arr) != 0)
+	{
+		sl_clear_map(arr);
+		sl_error_exit(EXIT_FAILURE, "Map is not rectangular.");
+	}
 }
